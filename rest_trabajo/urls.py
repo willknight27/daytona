b75108033fcfec82b932a9c9606e592822cfd0a3
Williams Caballero <knightex22@gmail.com>
2021-07-08 13:43:17 -0400

    Agregar funcion con metodos API - PUT y DELETE

--- rest_trabajo/urls.py
+++ rest_trabajo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from rest_trabajo.views import lista_trabajos
+from rest_trabajo.views import lista_trabajos,detalle_trabajo
 
 urlpatterns = [
     path('lista-trabajos',lista_trabajos,name='lista-trabajos'),
+    path('detalle-trabajo/<id>',detalle_trabajo, name = 'detalle-trabajo'),
 ]
--- rest_trabajo/views.py
+++ rest_trabajo/views.py
@@ -23,3 +23,24 @@ def lista_trabajos(request):
         else:
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET','PUT','DELETE'])
+def detalle_trabajo(request,id):
+
+    try:
+        trabajo = Trabajo.objects.get(id_trabajo=id)
+    except Trabajo.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = TrabajoSerializer(trabajo)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = TrabajoSerializer(trabajo, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        trabajo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
